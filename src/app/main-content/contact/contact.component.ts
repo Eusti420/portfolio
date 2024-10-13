b7b9f3adf247efb875e18b79c0f8e10221c1634c
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-10-13 17:28:43 +0200

    contact section added

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {merge} from 'rxjs';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+            MatFormFieldModule, 
+            MatSelectModule, 
+            MatInputModule,
+            FormsModule,
+            ReactiveFormsModule, 
+          ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
 
+export class ContactComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  errorMessage = '';
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage = 'You must enter a value';
+    } else if (this.email.hasError('email')) {
+      this.errorMessage = 'Not a valid email';
+    } else {
+      this.errorMessage = '';
+    }
+  }
 }
