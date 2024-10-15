0dc234b4d532e85ae412fe88e5b42d995e6e3108
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-10-15 16:12:27 +0200

    scss added

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,27 +1,53 @@
-import { Component } from '@angular/core';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {merge} from 'rxjs';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, signal, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { merge } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import {
+  MatBottomSheet,
+  MatBottomSheetModule,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
-            MatFormFieldModule, 
-            MatSelectModule, 
-            MatInputModule,
-            FormsModule,
-            ReactiveFormsModule, 
-          ],
+        MatButtonModule, 
+        MatListModule, 
+        MatBottomSheetModule, 
+        CommonModule, 
+        MatFormFieldModule, 
+        MatCheckboxModule, 
+        MatIconModule, 
+        MatInputModule, 
+        FormsModule, 
+        ReactiveFormsModule,
+      ],
+
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 
 export class ContactComponent {
+/*   http = inject(HttpClient)
+ */
+
+   contactData = {
+      name: "",
+      email: "",
+      message: "",
+      privacy: false
+   }  
+
   email = new FormControl('', [Validators.required, Validators.email]);
 
   errorMessage = '';
@@ -41,4 +67,10 @@ export class ContactComponent {
       this.errorMessage = '';
     }
   }
-}
+
+  onSubmit() {
+
+  }
+};
+
+
