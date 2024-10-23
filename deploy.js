1d796cf46194d11f8dbec218aa0dbd5697cfb951
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-10-23 11:47:30 +0200

    deploy.js

--- deploy.js
+++ deploy.js
@@ -1,14 +1,12 @@
 var ghpages = require('gh-pages');
 
-ghpages.publish('portfolio/dist/portfolio/browser', {
+ghpages.publish('dist/portfolio/browser', {
     branch: 'gh-pages',
     repo: 'https://github.com/Eusti420/portfolio.git',
-    cname: "home.feusterholz.de	",
+    cname: "feusterholz.de",
     history: false,
 }, function(err) {
     if (err) {
         console.error("Fehler beim Veröffentlichen:", err);
-    } else {
-        console.log("Deployment erfolgreich!");
     }
 });
