9a503fd99fcdcbcf977651bdf8d33481dfdf3205
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-10-28 12:58:32 +0100

    scss and imprint added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { SkillsComponent } from './main-content/skills/skills.component';
 import { PortfolioComponent } from './main-content/portfolio/portfolio.component';
 import { ContactComponent } from './main-content/contact/contact.component';
 import { HeroComponent } from './main-content/hero/hero.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
@@ -12,5 +14,7 @@ export const routes: Routes = [
     { path: 'skills', component: SkillsComponent },
     { path: 'portfolio', component: PortfolioComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'hero', component: HeroComponent }
+    { path: 'hero', component: HeroComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
