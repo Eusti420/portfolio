d9ef3c8b461d9c2db6213bdaab97773c4d4c1c2f
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-10-31 12:47:08 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
-import { AboutMeComponent } from './main-content/about-me/about-me.component';
-import { SkillsComponent } from './main-content/skills/skills.component';
-import { PortfolioComponent } from './main-content/portfolio/portfolio.component';
-import { ContactComponent } from './main-content/contact/contact.component';
-import { HeroComponent } from './main-content/hero/hero.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { NgModule } from '@angular/core';
@@ -12,14 +7,10 @@ import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
-    { path: 'about-me', component: AboutMeComponent },
-    { path: 'skills', component: SkillsComponent },
-    { path: 'portfolio', component: PortfolioComponent },
-    { path: 'contact', component: ContactComponent },
-    { path: 'hero', component: HeroComponent },
     { path: 'imprint', component: ImprintComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: '**', redirectTo: '' },
+
 ];
 
 @NgModule({
